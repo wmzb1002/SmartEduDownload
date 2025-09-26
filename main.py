47a832e0b914993827f71477962d626b7c4c9991
wmzb1002 <wmzb1002@outlook.com>
2025-09-26 23:12:03 +0800

    - 添加对输入URL中headers参数的解析支持 - 使用urllib替代requests库发起HTTP请求 - 实现文件流式下载，每次读取1024字节数据 - 支持从URL中自动提取默认文件名- 增加异常处理机制，提升程序健壮性- 添加下载完成后的3秒延时自动退出功能

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 # -*- coding: utf-8 -*-
-# v0.0.1#2
+# v0.0.2#1
 
-from urllib.parse import unquote
-
-import requests
+import re
+import time
+import urllib.parse
+import urllib.request
 
 url = input("请输入要下载的URL：")
-headers = {
-    "X-ND-AUTH": "MAC id=\"7F938B205F876FC398BCDC5BCE419D07BAC7D3514723EF4786BA8CDE8D9C6DBA85AEA50F8550B6F4D71391E168C655101B2CEADE63432EDC\",nonce=\"1758292663477:UJATFPSP\",mac=\"R9VZcOiRWzQFGiwxUiwKn83iOZImV6QZwRMalamYLv8=\""
-}
+try:
+    headers = eval(urllib.parse.unquote(url.split("&headers=")[1]))
 
-response = requests.get(url, headers=headers)
-if response.status_code == 200:
-    filename = input("请输入保存的文件名：[{}]".format(unquote(url.split('/')[-1])))
-    with open(filename if filename else '{}'.format(unquote(url.split('/')[-1])), "wb") as f:
-        f.write(response.content)
-    print("下载成功！")
-else:
-    print("下载失败，状态码：", response.status_code)
+    requests = urllib.request.Request(url, headers=headers)
+    response = urllib.request.urlopen(requests)
+
+    if response.status == 200:
+        filename = input(
+            "请输入保存的文件名：[{}]".format(
+                urllib.parse.unquote(
+                    re.search(r"https(.*)pdf", url).group().split("/")[-1]
+                )
+            )
+        )
+        with open(
+            (
+                filename
+                if filename
+                else "{}".format(
+                    urllib.parse.unquote(
+                        re.search(r"https(.*)pdf", url).group().split("/")[-1]
+                    )
+                )
+            ),
+            "wb",
+        ) as f:
+            data = response.read(1024)  # 每次读取1024字节
+            while data:
+                f.write(data)
+                data = response.read(1024)
+        print("下载成功！3秒后退出程序。。。")
+    else:
+        print("下载失败，状态码：", response.status_code)
+except Exception as e:
+    print("错误！请检查链接、保存路径是否正确或此版本是否有效。错误码：", e)
+
+time.sleep(3)
